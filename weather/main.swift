e569bb842d7f624e738a052280193a3b74de1348
dant <dant@dants-iMac.local>
2019-04-29 20:31:49 +0200

    adapte swift5

--- weather/main.swift
+++ weather/main.swift
@@ -10,7 +10,7 @@ let runLoop = CFRunLoopGetCurrent()
 extension URL {
     func withQueries(_ queries: [String: String]) -> URL? {
         var components = URLComponents(url: self, resolvingAgainstBaseURL: true)
-        components?.queryItems = queries.flatMap {
+        components?.queryItems = queries.compactMap {
             URLQueryItem(name: $0.0, value: $0.1)
         }
         return components?.url
