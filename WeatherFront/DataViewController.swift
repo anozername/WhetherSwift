9d70daafe2fe0939dbda4ca551ec66162af80c80
dont@send.mail <dont@send.mail>
2019-05-14 20:48:50 +0200

    debut p2

---
+++ WeatherFront/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  WeatherFront
+//
+//  Created by dant on 13/05/2019.
+//  Copyright © 2019 dant. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
