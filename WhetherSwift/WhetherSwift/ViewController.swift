469307f0469e078e83b248690ddf317c03907388
dont@send.mail <dont@send.mail>
2019-05-24 18:06:33 +0200

    framework WIP

--- WhetherSwift/WhetherSwift/ViewController.swift
+++ WhetherSwift/WhetherSwift/ViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import Weather
 
 class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var InputField: UITextField!
     @IBOutlet weak var TextZone: UITextView!
     let defaults = UserDefaults.standard
+    let weatherClient = WeatherClient(key: "e7a6caa465aee8f94b57375dde1ba754")
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,8 +31,18 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
 
     @IBAction func SubmitButton(_ sender: UIButton) {
+
+        var something = ""
+        let task = weatherClient.weather(for: weatherClient.citiesSuggestions(for: "Paris")[0], completion: { data in
+            if let data = data {
+                //print(data) ok
+                something = String(decoding: data, as: UTF8.self)
+            } else {
+                // no data and no error... what happened???
+            }
+        })
         if let input = defaults.string(forKey: "input"){
-            TextZone.text = input
+            TextZone.text = something
         }
     }
     
