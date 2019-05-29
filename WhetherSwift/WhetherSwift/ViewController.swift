bd777fdebe453a81b16bf1f14a969002012dd2fa
dont@send.mail <dont@send.mail>
2019-05-29 17:24:33 +0200

    Suggestion feature

--- WhetherSwift/WhetherSwift/ViewController.swift
+++ WhetherSwift/WhetherSwift/ViewController.swift
@@ -6,50 +6,81 @@
 //  Copyright © 2019 dant. All rights reserved.
 //
 
+//  TODO debug touch screen
+
 import UIKit
 import Weather
 
 class ViewController: UIViewController, UITextFieldDelegate {
-    @IBOutlet weak var InputField: UITextField!
+    @IBOutlet weak var InputField: SearchTextField!
     @IBOutlet weak var TextZone: UITextView!
     @IBOutlet weak var Spinn: UIActivityIndicatorView!
-    let defaults = UserDefaults.standard
-    let weatherClient = WeatherClient(key: "e7a6caa465aee8f94b57375dde1ba754") // d4398ab9c5924d563949cf24f6881e50 or e7a6caa465aee8f94b57375dde1ba754
+    var input = ""
+    let weatherClient = WeatherClient(key: "d4398ab9c5924d563949cf24f6881e50") // d4398ab9c5924d563949cf24f6881e50 or e7a6caa465aee8f94b57375dde1ba754
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
         InputField.delegate = self
+        self.InputField.filterStrings(["None"])
+        self.InputField.itemSelectionHandler = {item, itemPosition in
+            self.InputField.text = item[itemPosition].title
+            self.input = item[itemPosition].title
+        }
     }
+    
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
+        print("textFieldShouldReturn")
         return true
     }
-    func textFieldDidEndEditing(_ textField: UITextField){
-        defaults.set(textField.text, forKey: "input")
+    
+    // It is called each time user type a character by keyboard
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if range.length != 0 {
+            for _ in 1...range.length {
+                input = String(input.dropLast())
+            }
+        }
+        input += string
+        print("input :'\(input)',string :'\(string)', range : '\(range)', length : \(range.length)")
+        
+        var suggestion = ""
+        var suggestions = [""]
+        for city in weatherClient.citiesSuggestions(for: input) {
+            suggestion = "\(city.name)"
+            suggestions.append(suggestion)
+        }
+        self.InputField.filterStrings(suggestions)
+
+        return true
     }
 
     @IBAction func SubmitButton(_ sender: UIButton) {
         Spinn.startAnimating()
         let semaphore = DispatchSemaphore(value: 0)
         TextZone.text = " "
+    
         var result = "Message ?"
-        let task = weatherClient.weather(for: weatherClient.citiesSuggestions(for: "Paris")[0], completion: { response in
+        let task = weatherClient.weather(for: weatherClient.citiesSuggestions(for: input)[0], completion: { response in
             if let data = response {
                 print("response ok")
                 self.TextZone.text = "Message : \(data)" // 63 sec pending
                 semaphore.signal()
-                self.Spinn.stopAnimating()
             }
         })
         semaphore.wait()
-        
-        if let input = defaults.string(forKey: "input"){
-            // TextZone.text = result
-            print("submitted")
+
+        if input == "klknlk" {
+            print("result is at its default value")
+        }else if input == "" || input == " "{
+            print("result is empty")
+        }else{
+            print("result : '\(input)'")
         }
+        Spinn.stopAnimating()
     }
 }
 
